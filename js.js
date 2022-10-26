914c7679350885ac5745d70f4291aa804736152f
fitoplank <68260274+fitoplank@users.noreply.github.com>
2022-10-26 20:26:51 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+// es6 ECMA script 
+// no tiene metodos deprecados
+// free code cams (aprende javascript)
+// educacion it 
+// landing page
+// moquear
+//regex
+// xss
+// token 
+// cookee
+
+
+var verdulería = ["ajo", "cebolla", "tomate", "banana", "manzana", "pera", "durazno"];
+console.log(verdulería.length); 
+verdulería.push("acelga");
+alert("funciona!")
+console.log(verdulería.length);
+
